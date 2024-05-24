381a3f6516bf6740890f78a1f884c6b781f6b9df
Denise148 <167827528+Denise148@users.noreply.github.com>
2024-05-23 18:38:54 -0600

    Coded some ja

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let albuquerqueElement = document.querySelector("#albuquerque");
+  let albuquerqueDateElement =
+    albuquerqueElement.querySelector("#albuquerque-date");
+  let albuquerqueTimeElement =
+    albuquerqueElement.querySelector("#albuquerque-time");
+  let albuquerqueTime = moment().tz("America/Denver");
 
+  albuquerqueDateElement.innerHTML = albuquerqueTime.format("MMMM Do YYYY");
+  albuquerqueTimeElement.innerHTML = albuquerqueTime.format(
+    "h:mm:ss[<small>]A[</small]>"
+  );
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector("#paris-date");
+  let parisTimeElement = parisElement.querySelector("#paris-time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>]A[</small]>");
+}
+updateTime();
+setInterval(updateTime, 1000);
